a06c5221bc2a780bf01e13c1c6d61377db59d40c
jpoezi <juanpytf2@gmail.com>
2023-01-14 17:55:54 -0300

    base style for login page

---
+++ src/pages/auth/login.js
@@ -0,0 +1,19 @@
+import {FcGoogle} from 'react-icons/fc'
+
+
+function Login() {
+  return (
+    <div className="shadow-xl mt-32 p-10 text-gray-700 rounded-lg">
+      <h2 className="text-2xl font-medium"> Join Today </h2>
+
+      <div className="py-4">
+        <h3 className="py-4"> Sign in with one of the providers</h3>
+        <button className="text-[#f0f0f0] bg-gray-700 w-full font-medium rounded-lg flex align-middle p-4"> 
+        <FcGoogle className="mr-2 text-2xl" /> Sign in with Google 
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default Login
