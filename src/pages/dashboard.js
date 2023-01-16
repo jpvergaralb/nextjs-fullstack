4c790d2448eb3c5df851443015584784080344e9
jpoezi <juanpytf2@gmail.com>
2023-01-16 17:15:16 -0300

    add delete own post functionality

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -3,9 +3,12 @@ import {auth} from '../../utils/firebase'
 import {useAuthState} from 'react-firebase-hooks/auth'
 import { useRouter } from 'next/router'
 import { useEffect, useState } from 'react'
-import { collection, query, where, onSnapshot } from 'firebase/firestore'
-import { db } from '../../utils/firebase'
+import { collection, query, where, onSnapshot, doc, docRef, deleteDoc } from 'firebase/firestore'
+import { db} from '../../utils/firebase'
 import Message from '@/components/message'
+import {BsTrash2Fill} from 'react-icons/bs'
+import {AiFillEdit} from 'react-icons/ai'
+import {GoSignOut} from 'react-icons/go'
 
 // to fix the flashing data when redirecting 
 //https://theodorusclarence.com/blog/nextjs-redirect-no-flashing
@@ -20,6 +23,13 @@ function Dashboard() {
   
   // Router
   const router = useRouter()
+
+  // Delete posts
+  const deletePost = async (id) => {
+    const docRef = doc(db, 'posts', id)
+    await deleteDoc(docRef)
+  }
+
 
   const getData = async () => {
     if (loading) return (<h1> Loading... </h1>)
@@ -46,13 +56,22 @@ function Dashboard() {
     <h1> Your posts </h1>
 
     <div>
-      {allPosts.map((post) => <Message key={post.id} {...post} />)}
+      {allPosts.map((post) =>
+                          <Message key={post.id} {...post}>
+                              <div className='flex flex-start items-center my-3 '> 
+                                <button className='flex items-center gap-3 px-4  bg-gradient-to-r from-indigo-400 to-slate-600 rounded-md p-1 text-[#f0f0f0]' onClick={() => deletePost(post.id)}> Delete <BsTrash2Fill className='text-md' /> </button> 
+                                <button className='flex items-center gap-3 px-4 mx-4 bg-gradient-to-r from-indigo-400 to-slate-600 rounded-md p-1 text-[#f0f0f0]' onClick={() => { }}> Edit <AiFillEdit/></button> 
+                              </div>
+                            </Message>
+                              )}
     </div>
 
     <button onClick={() => {
       auth.signOut()
-    }}> 
+    }}
+      className="flex items-center gap-3 p-2 bg-slate-900 rounded-md text-[#f0f0f0] my-10 "> 
       Sign out 
+      <GoSignOut className='text-md' />
     </button>
 
     </div>
