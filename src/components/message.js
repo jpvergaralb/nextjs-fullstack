1436e382c07f83360e027f03e7452db8cf96d070
jpoezi <juanpytf2@gmail.com>
2023-01-16 17:04:48 -0300

    modify size of avatar

--- src/components/message.js
+++ src/components/message.js
@@ -5,8 +5,8 @@ function Message({children, avatar, username, description, timestamp, id}) {
     <div className="bg-white p-8 border-b-2 rounded-lg">
 
       <div className="flex flex-center justify-start">
-        <Image src={avatar} height={32} width={32} className="rounded-md " alt="You shouldn't see this"></Image>
-        <h2 className="mx-2 text-transparent bg-clip-text bg-gradient-to-r from-black to-slate-600 font-medium md:text-4xl dark:from-violet-900 dark:to-blue-400"> {username} </h2>
+        <Image src={avatar} height={42} width={42} className="rounded-full " alt="You shouldn't see this"></Image>
+        <h2 className="flex items-center mx-2 text-transparent bg-clip-text bg-gradient-to-r from-black to-slate-600 font-medium dark:from-violet-900 dark:to-blue-400"> {username} </h2>
       </div>
       <div className="flex flex-start">
           <h3 className="text-ellipsis overflow-clip font-normal py-3"> {description} </h3>
