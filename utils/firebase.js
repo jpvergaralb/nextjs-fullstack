0c747ace23cb71c505e45ca332da7a445409f0b6
jpoezi <juanpytf2@gmail.com>
2023-01-16 03:37:43 -0300

    small change

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
 //Get auth
 import {getAuth} from 'firebase/auth' 
@@ -24,7 +25,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-export const auth = getAuth(app)
-export const bd = getFireStore(app)
+export const auth = getAuth();
+export const db = getFirestore(app);
